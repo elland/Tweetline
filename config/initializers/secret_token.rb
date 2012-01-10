b1c8b54c606acdfdce1153a3b1f978bc81953862
Igor Ranieri Elland <eu@elland.me>
2012-01-10 15:09:47 -0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetline::Application.config.secret_token = '16fbadb77c462698f823e9d36529ddff1abadafc5af5b500d9f4524b2aef1cc7bbd3a82ac54d0730c59727c7a793ac3590fc2fa608e569c86488b83b7b9cf3d0'
